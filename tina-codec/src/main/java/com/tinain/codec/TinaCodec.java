20f9dacabdecbd6bb4193ad9a1799f9840b9ce09
Alan Lau <wayilau@gmail.com>
2019-08-13 17:41:15 +0800

    Add codec logic.

--- tina-codec/src/main/java/com/tinain/codec/TinaCodec.java
+++ tina-codec/src/main/java/com/tinain/codec/TinaCodec.java
@@ -13,31 +13,62 @@ import java.util.List;
  */
 public class TinaCodec extends ByteToMessageCodec<TinaMessage> {
 
+    private TinaMessage message;
+
     private Serializer serializer;
 
-    @Override protected void encode(ChannelHandlerContext ctx, TinaMessage msg, ByteBuf out) throws Exception {
 
+    @Override protected void encode(ChannelHandlerContext ctx, TinaMessage msg, ByteBuf out) throws Exception {
+        if (msg == null) {
+            return;
+        }
+
+        out.writeInt(msg.getMajor());
+        out.writeShort(msg.getVersion());
+        out.writeByte(msg.getType());
+        out.writeShort(msg.getLength());
+        out.writeBytes(serializer.serializer(msg.getObject()));
+        out.writeInt(msg.getCsc());
     }
 
     @Override protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 
+        if (message == null) {
+            message = new TinaMessage();
+        }
+
         if (!in.isReadable()) {
             return;
-        }else {
-            int major = in.readInt();
-            byte version = in.readByte();
-            byte type = in.readByte();
-            short length = in.readShort();
-
-            byte[] bytes = new byte[]{};
-            in.readBytes(bytes, 5, in.capacity() - 4);
-
-            Object obj = serializer.deserializer(bytes);
-
-            int crc = in.readInt();
-
-            TinaMessage tm = new TinaMessage();
-
         }
+
+        if (in.readableBytes() > 4 && message.getMajor() == 0) {
+            message.setMajor(in.readInt());
+        }
+
+        if (in.readableBytes() > 2 && message.getVersion() == 0) {
+            message.setVersion(in.readShort());
+        }
+
+        if (in.readableBytes() > 1 && message.getType() == 0) {
+            message.setType(in.readByte());
+        }
+
+        if (in.readableBytes() > 2 && message.getLength() == 0) {
+            message.setLength(in.readShort());
+        }
+
+        if (in.readableBytes() > message.getLength() && message.getObject() == null) {
+            message.setObject(serializer.deserializer(in.readBytes(message.getLength()).array()));
+        }
+
+        if (in.readableBytes() > 4 && message.getCsc() == 0) {
+            message.setCsc(in.readInt());
+        }
+
+        if (message.getCsc() != 0) {
+            out.add(message);
+            message = null;
+        }
+
     }
 }
